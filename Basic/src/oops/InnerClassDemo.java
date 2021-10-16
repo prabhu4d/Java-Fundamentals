9ef6c8870823964652e187919a9ef5d316048d29
Prabhu <programmerprabhu@gmail.com>
2021-10-16 17:40:33 +0530

    Inner Class

---
+++ Basic/src/oops/InnerClassDemo.java
@@ -0,0 +1,18 @@
+package oops;
+
+class Outer{
+	int a;
+	public void show() {
+		
+	}
+}
+
+public class InnerClassDemo {
+
+	public static void main(String[] args) {
+		Outer obj = new Outer();
+		obj.show();
+		
+	}
+
+}
